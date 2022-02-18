d24e1ce25984b15830cfefa6414349f89f103006
deepa <deepakarra@gmail.com>
2022-02-18 23:08:44 +0100

    Adding test case

--- src/test/java/com/employee/controller/EmployeeControllerTest.java
+++ src/test/java/com/employee/controller/EmployeeControllerTest.java
@@ -3,6 +3,7 @@ package com.employee.controller;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Order;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.employee.model.Employee;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -23,11 +26,37 @@ public class EmployeeControllerTest {
     @Autowired
 	protected MockMvc mvc;
     
-	
 	@Test
+	@Order(1)
+	public void createsEmployeeSuccessfully()  throws Exception{
+	String uri = "/employees"	;
+	
+	Employee emp = new Employee();
+	emp.setEmployeeName("emp1");
+	emp.setCity("Ams");
+	String requestBody ="{\r\n"
+			+ "\"employeeName\":\"testUser5\",\r\n"
+			+ "\"city\":\"ams\"\r\n"
+			+ "}";
+	String responseBody ="{\r\n"
+			+ "\"employeeid\":\"1\",\r\n"
+			+ "\"employeeName\":\"testUser5\",\r\n"
+			+ "\"city\":\"ams\"\r\n"
+			+ "}";
+	MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn();
+	assertEquals(201,mvcResult.getResponse().getStatus());
+}
+    
+	@Test
+	@Order(2)
 	public void returnsEmployeesSuccessfully()  throws Exception{
 	String uri = "/employees"	;
+	String responseBody ="{\r\n"
+			+ "\"employeeName\":\"testUser5\",\r\n"
+			+ "\"city\":\"ams\"\r\n"
+			+ "}";
 	MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON)).andReturn();
 	assertEquals(200,mvcResult.getResponse().getStatus());
+	
 }
 }
