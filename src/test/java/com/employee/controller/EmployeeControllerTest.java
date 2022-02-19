1ed55f595277e3e9e4edf8f6a25b1974f872a961
deepa <deepakarra@gmail.com>
2022-02-19 13:58:06 +0100

    Adding more test cases

--- src/test/java/com/employee/controller/EmployeeControllerTest.java
+++ src/test/java/com/employee/controller/EmployeeControllerTest.java
@@ -4,6 +4,7 @@ package com.employee.controller;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Order;
 import org.junit.runner.RunWith;
@@ -15,6 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.core.Is.is;
 
@@ -28,7 +32,16 @@ public class EmployeeControllerTest {
 	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 	@Autowired
 	protected MockMvc mvc;
-
+	
+	@Before
+	public void setupTestdata() throws Exception {
+		String uri = "/employees";
+		String requestBody = "{\r\n" + "\"employeeName\":\"emp1\",\r\n" + "\"city\":\"Ams\"\r\n" + "}";
+		mvc.perform(MockMvcRequestBuilders.post(uri).
+				contentType(MediaType.APPLICATION_JSON).
+				content(requestBody));
+	}
+	
 	@Test
 	@Order(1)
 	public void createsEmployeeSuccessfully() throws Exception {
@@ -41,7 +54,7 @@ public class EmployeeControllerTest {
 						contentType(MediaType.APPLICATION_JSON).
 						content(requestBody))
 						.andExpect(status().isCreated())
-						.andExpect(content().json("{'employeeid':2, 'employeeName':'emp1','city':'Ams'}"))
+						.andExpect(content().json("{'employeeid':4, 'employeeName':'emp1','city':'Ams'}"))
 						.andReturn();		
 	}
 
@@ -49,13 +62,34 @@ public class EmployeeControllerTest {
 	@Order(2)
 	public void returnsEmployeesSuccessfully() throws Exception {
 		String uri = "/employees";
-		String requestBody = "{\r\n" + "\"employeeName\":\"emp1\",\r\n" + "\"city\":\"Ams\"\r\n" + "}";
-		mvc.perform(MockMvcRequestBuilders.post(uri).
-				contentType(MediaType.APPLICATION_JSON).
-				content(requestBody));
 		mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().is2xxSuccessful())
 				.andExpect(content().json("[{'employeeid':1, 'employeeName':'emp1','city':'Ams'}]"))
+				.andReturn();
+		
+	}
+	
+	@Test
+	@Order(3)
+	public void getEmployeeByIdSuccessfully() throws Exception {
+		String uri = "/employees/1" ;
+		mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(content().json("{'employeeid':1, 'employeeName':'newEmp','city':'Ams'}"))
+				.andReturn();
+		
+	}
+	
+	@Test
+	@Order(4)
+	public void updatesEmployeeDetailsSuccessfully() throws Exception {
+		String uri = "/employees" ;
+		String updateuri = "/employees/1" ;
+		String newRequestBody = "{\r\n" + "\"employeeName\":\"newEmp\",\r\n" + "\"city\":\"Ams\"\r\n" + "}";
+		mvc.perform(MockMvcRequestBuilders.put(updateuri).contentType(MediaType.APPLICATION_JSON)
+		        .content(newRequestBody))
+			.andExpect(status().is2xxSuccessful())
+	         	.andExpect(content().json("{'employeeid':1, 'employeeName':'newEmp','city':'Ams'}"))
 				.andReturn();
 		
 	}
