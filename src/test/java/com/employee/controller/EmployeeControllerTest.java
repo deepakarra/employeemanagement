49b9895eb75f4737bea6e2b6de126a0194573e45
deepa <deepakarra@gmail.com>
2022-02-19 09:00:27 +0100

    verifying responsebody in junits and updating springboot version

--- src/test/java/com/employee/controller/EmployeeControllerTest.java
+++ src/test/java/com/employee/controller/EmployeeControllerTest.java
@@ -1,6 +1,8 @@
 package com.employee.controller;
 
+//import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Order;
@@ -9,54 +11,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.core.Is.is;
 
 import com.employee.model.Employee;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 public class EmployeeControllerTest {
-	
-    @Autowired
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+	@Autowired
 	protected MockMvc mvc;
-    
+
 	@Test
 	@Order(1)
-	public void createsEmployeeSuccessfully()  throws Exception{
-	String uri = "/employees"	;
-	
-	Employee emp = new Employee();
-	emp.setEmployeeName("emp1");
-	emp.setCity("Ams");
-	String requestBody ="{\r\n"
-			+ "\"employeeName\":\"testUser5\",\r\n"
-			+ "\"city\":\"ams\"\r\n"
-			+ "}";
-	String responseBody ="{\r\n"
-			+ "\"employeeid\":\"1\",\r\n"
-			+ "\"employeeName\":\"testUser5\",\r\n"
-			+ "\"city\":\"ams\"\r\n"
-			+ "}";
-	MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andReturn();
-	assertEquals(201,mvcResult.getResponse().getStatus());
-}
-    
+	public void createsEmployeeSuccessfully() throws Exception {
+		String uri = "/employees";
+		Employee emp = new Employee();
+		emp.setEmployeeName("emp1");
+		emp.setCity("Ams");
+		String requestBody = "{\r\n" + "\"employeeName\":\"emp1\",\r\n" + "\"city\":\"Ams\"\r\n" + "}";
+				mvc.perform(MockMvcRequestBuilders.post(uri).
+						contentType(MediaType.APPLICATION_JSON).
+						content(requestBody))
+						.andExpect(status().isCreated())
+						.andExpect(content().json("{'employeeid':2, 'employeeName':'emp1','city':'Ams'}"))
+						.andReturn();		
+	}
+
 	@Test
 	@Order(2)
-	public void returnsEmployeesSuccessfully()  throws Exception{
-	String uri = "/employees"	;
-	String responseBody ="{\r\n"
-			+ "\"employeeName\":\"testUser5\",\r\n"
-			+ "\"city\":\"ams\"\r\n"
-			+ "}";
-	MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON)).andReturn();
-	assertEquals(200,mvcResult.getResponse().getStatus());
-	
-}
+	public void returnsEmployeesSuccessfully() throws Exception {
+		String uri = "/employees";
+		String requestBody = "{\r\n" + "\"employeeName\":\"emp1\",\r\n" + "\"city\":\"Ams\"\r\n" + "}";
+		mvc.perform(MockMvcRequestBuilders.post(uri).
+				contentType(MediaType.APPLICATION_JSON).
+				content(requestBody));
+		mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(content().json("[{'employeeid':1, 'employeeName':'emp1','city':'Ams'}]"))
+				.andReturn();
+		
+	}
 }
